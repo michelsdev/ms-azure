b1dd60d4ed4ed4a1f2860bb6dd80bc2d8a4a538d
michelsdev <fernando.michels@gmail.com>
2020-01-26 20:27:04 -0300

    Change path and create new folder to organizate

--- azure-pipeline-vm-creation/Scripts/create-vm.ps1
+++ azure-pipeline-vm-creation/Scripts/create-vm.ps1
@@ -1,3 +1,4 @@
 param ([string]$vmname,[string]$user,[string]$pass)
 az group deployment create --resource-group rg-lab-devops --template-file .\azure-pipeline-vm-creation\templates\WindowsVirtualMachine.json --parameters .\azure-pipeline-vm-creation\templates\WindowsVirtualMachine.parameters.json --parameters adminUsername=$user --parameters adminPassword=$pass --parameters virtualMachineName=$vmname
+az vm deallocate -g rg-lab-devops -n $vmname
 
